9839fd0bf8086a8ae09c9b68446a368310b7ecfa
Yash Kumar Sugandhi <85982995+ykstech@users.noreply.github.com>
2021-09-02 14:51:19 +0530

    Update Constants.php

--- HeroApi/includes/Constants.php
+++ HeroApi/includes/Constants.php
@@ -5,7 +5,7 @@
 	* website: www.simplifiedcoding.net 
 	*/
 	
-	define('DB_HOST', 'fdb17.biz.nf');
-	define('DB_USER', '3355501_login');
-	define('DB_PASS', 'yash5364');
-	define('DB_NAME', '3355501_login');
+	define('DB_HOST', '');
+	define('DB_USER', '');
+	define('DB_PASS', '');
+	define('DB_NAME', '');
