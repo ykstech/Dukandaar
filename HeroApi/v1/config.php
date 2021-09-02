aac605f2eebd08d326bc0c3b288e0f807aa8e9f0
Yash Kumar Sugandhi <85982995+ykstech@users.noreply.github.com>
2021-09-02 14:48:14 +0530

    Update config.php

--- HeroApi/v1/config.php
+++ HeroApi/v1/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$dbHost     = "fdb17.biz.nf";
-$dbUsername = "3355501_login";
-$dbPassword = "yash5364";
-$dbName     = "3355501_login";
+$dbHost     = "";
+$dbUsername = "";
+$dbPassword = "";
+$dbName     = "";
 
 // Create database connection
 $conn=mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
